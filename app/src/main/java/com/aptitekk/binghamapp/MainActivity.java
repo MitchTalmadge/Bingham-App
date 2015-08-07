efafb3ba399e112b0f3a7f3552a3a5de899f66b7
Mitch Talmadge <mitch@mitchtalmadge.com>
2015-08-07 13:21:48 -0600

    Replaced ActionBar with Toolbar, added new logo

--- app/src/main/java/com/aptitekk/binghamapp/MainActivity.java
+++ app/src/main/java/com/aptitekk/binghamapp/MainActivity.java
@@ -1,17 +1,26 @@
 package com.aptitekk.binghamapp;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp((DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
     }
 
     @Override
