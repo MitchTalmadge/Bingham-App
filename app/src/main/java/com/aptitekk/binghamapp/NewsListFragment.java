e5e26a27dfd3c4338b3aad8709d599fdcb3dc45e
Kevin Thorne <kevin@kevinpthorne.com>
2015-08-09 14:09:26 -0600

    Fixed rssnewsfeed package name

--- app/src/main/java/com/aptitekk/binghamapp/NewsListFragment.java
+++ app/src/main/java/com/aptitekk/binghamapp/NewsListFragment.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.aptitekk.binghamapp.rssNewsFeed.NewsArticle;
+import com.aptitekk.binghamapp.rssnewsfeed.NewsArticle;
 
 import java.util.ArrayList;
 import java.util.List;
--- app/src/main/java/com/aptitekk/binghamapp/SchoolNewsFragment.java
+++ app/src/main/java/com/aptitekk/binghamapp/SchoolNewsFragment.java
@@ -10,7 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.aptitekk.binghamapp.rssNewsFeed.RSSNewsFeed;
+import com.aptitekk.binghamapp.rssnewsfeed.RSSNewsFeed;
 
 import java.util.concurrent.Callable;
 
